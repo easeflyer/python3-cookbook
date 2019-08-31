e7c67c2d4c228aed7645440e2fb1dee8af6539b9
ease <easenet@gmail.com>
2019-08-31 10:10:00 +0800

    在家补充的案例描述

--- cookbook/c01/p03_nitem.py
+++ cookbook/c01/p03_nitem.py
@@ -4,23 +4,33 @@
 Topic: collections.deque演示
 Desc : deque有一个maxlen参数，当append的时候，如果超过，那么最前面的就被挤出队列。
 
-题目：搜索某个字符串，找到后输出最后5行以及包含字符串的行。也就是最后6行
+题目：保留最后有限几个元素的历史记录
 知识点：
-    1 collections.deque 保留有限成员，其他被挤出
-    2 注意 deque 比列表操作更快。时间复杂度：O(1) O(n)
+    1 通过 open 打开纯文本文件。可以对文件进行迭代，每次获得一行。
+    2 list(open(filename)) 可以吧文件以行的形式保存成 list 
+    3 collections.deque 保留有限成员，其他被挤出
+    4 注意 deque 比列表操作更快。时间复杂度：O(1) O(n)
 """
 from collections import deque
 
 
 def search(lines, pattern, history=5):
+    '''
+    输入：
+        lines           就是 文本文件对象。
+        pattern         是匹配模式
+        history         仅保留5条记录
+    返回：
+        li              一行文本
+        previous_lines  保存在队列里的行
+    '''
     previous_lines = deque(maxlen=history)
     for li in lines:
         if pattern in li:
             yield li, previous_lines
         previous_lines.append(li)
 
-# Example use on a file 234234   234234 
-if __name__ == '__main__':
+def test1():
     with open(r'../../cookbook/somefile.txt') as f:
         for line, prevlines in search(f, 'Python', 5):
             for pline in prevlines:
@@ -29,4 +39,15 @@ if __name__ == '__main__':
             print('-' * 20)
 
 
+def test0():
+    with open(r'../../cookbook/somefile.txt') as f:
+        for line in f:
+            print(line,end="")
+
+    filelist = list(open(r'../../cookbook/somefile.txt'))
+    print(filelist)
+
+# Example use on a file
+if __name__ == '__main__':
+    test1()
 
--- cookbook/c01/p04_nlargest.py
+++ cookbook/c01/p04_nlargest.py
@@ -5,16 +5,61 @@ Topic: 返回集合中最大或最小的N个元素
 Desc : 
 
 关于 堆排序的基础知识：https://www.cnblogs.com/clnchanpin/p/7217950.html
+参考练习：https://www.jianshu.com/p/801318c77ab5
+创建堆
+
+方法一：heapq.heappush 把数值逐一加入堆 heapq1()
+
+方法二：heap.heapify(list) 转换列表为 堆结构。
+
 """
 import heapq
 
 
+
+
+def heapq1():
+    """
+    题目：heapq.heappush 把数值逐一加入堆
+    用 _ 表示无用的数据变量
+    heap 为空堆数组。利用  heapq.heappush 加入数据。注意加入后的数据规则。
+    heap[0] 永远都是最小值
+    """
+    nums = [2, 3, 5, 1, 54, 23, 132]
+    heap = []
+    for num in nums:
+        heapq.heappush(heap, num)  # 加入堆
+
+    print(heap[0])  # 如果只是想获取最小值而不是弹出，使用heap[0]
+    print(heap)
+    print([heapq.heappop(heap) for _ in range(len(nums))])  # 堆排序结果
+
+def heapq2():
+    """
+    通过 heapq.heapify(nums) 把列表直接
+    """
+    nums = [2, 3, 5, 1, 54, 23, 132]
+    heapq.heapify(nums)
+    print("heap_list:",nums)
+    print("sort_list:",[heapq.heappop(nums) for _ in range(len(nums))])  # 堆排序结果    
+
+
+
 def test1():
+    """
+    利用 heapq 模块，从列表中取出 最大3个数，最小3个数
+    """
     nums = [1, 8, 2, 23, 7, -4, 18, 23, 42, 37, 2]
     print(heapq.nlargest(3, nums)) # Prints [42, 37, 23]
     print(heapq.nsmallest(3, nums)) # Prints [-4, 1, 2]    
 
 def main():
+    """
+    知识点：
+        1 nsmallest(n,list,key) 从列表list中取出n个最小的元素
+        2 nlargest(n,list,key)  从列表list中取出n个最大的元素
+        3 key 参数：如果list 元素是dict，则以 key为比较的字典元素，这里是比较 price
+    """
     portfolio = [
         {'name': 'IBM', 'shares': 100, 'price': 91.1},
         {'name': 'AAPL', 'shares': 50, 'price': 543.22},
@@ -25,8 +70,8 @@ def main():
     ]
     cheap = heapq.nsmallest(3, portfolio, key=lambda s: s['price'])
     expensive = heapq.nlargest(3, portfolio, key=lambda s: s['price'])
-    print(cheap)
-    print(expensive)
+    print("cheap:",cheap)
+    print("expensive:",expensive)
 
     nums = [1, 8, 2, 23, 7, -4, 18, 23, 42, 37, 2]
     heapq.heapify(nums)
@@ -37,6 +82,8 @@ def main():
 
 
 if __name__ == '__main__':
-    test1()
-    #main()
+    #heapq1()
+    #heapq2()
+    #test1()
+    main()
 
